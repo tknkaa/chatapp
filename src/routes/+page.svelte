a8dc558b4e504cdf16be6106c40ce5db3b343b13
tknkaa <tknkaa@users.noreply.github.com>
2026-03-28 23:06:49 +0800

    top page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
-  
+	import { goto } from '$app/navigation';
+	import { authClient } from '$lib/auth-client';
+	import { onMount } from 'svelte';
+
+	let isLoading = $state(true);
+	onMount(async () => {
+		const { data: session } = await authClient.getSession();
+		if (session) {
+			goto('/chat');
+		} else {
+			isLoading = false;
+		}
+	});
 </script>
 
 <h1>Chat App</h1>
+
+{#if isLoading}
+	<div>loading</div>
+{:else}
+	<div>
+		<button onclick={() => goto('/login')}> login </button>
+	</div>
+
+	<div>
+		<button onclick={() => goto('/signup')}> signup </button>
+	</div>
+{/if}
